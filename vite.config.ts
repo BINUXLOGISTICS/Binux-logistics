80c55161886825d779abc52947f75bd95a7ffec7
ShyamGurunath <shyamgurunath474@gmail.com>
2025-12-23 17:53:52 +0530

    [UPDATED][BASEPATH]

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/binux-logistics/', // Change this to your repo name
+  base: '/Binux-logistics/', // Must match exact repo name (case-sensitive)
 })
